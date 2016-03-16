594b8d093ac4387a83d052022a059363fbee9905
Alejandro Gallegos Tovar <a01154838@itesm.mx>
2016-03-16 18:22:58 +0000

    View for log in form was made

--- views/login_form.php
+++ views/login_form.php
@@ -1,13 +1,14 @@
+<body style="background-color:#bfbfbf"></body>
 <form action="login.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+            <input autocomplete="off" focus class="btn btn-danger" name="username" placeholder="Username" type="text"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password"/>
+            <input class="btn btn-danger" name="password" placeholder="Password" type="password"/>
         </div>
         <div class="form-group">
-            <button class="btn btn-default" type="submit">
+            <button class="btn btn-primary" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
                 Log In
             </button>
