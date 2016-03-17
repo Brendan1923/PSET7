58dc64bbc6c63dc2d738d168273df4752f3ecfec
Alejandro Gallegos Tovar <a01154838@itesm.mx>
2016-03-17 07:22:48 +0000

    The boxes for name and last name had been added

--- views/register_form.php
+++ views/register_form.php
@@ -1,16 +1,24 @@
+<body style="background-color:#bfbfbf">
 <form action="register.php" method="post">
+    <h1>Please, fill in the requirements</h1>
     <fieldset>
         <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+            <input autocomplete="off" focus class="btn btn-danger" name="name" placeholder="Name" type="text"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password"/>
+            <input autocomplete="off" focus class="btn btn-danger" name="last-name" placeholder="Last name" type="text"/>
         </div>
         <div class="form-group">
-            <input class ="form-control" name ="confirm" placeholder="Confirmation" type="password"/>
+            <input autocomplete="off" focus class="btn btn-danger" name="username" placeholder="Username" type="text"/>
         </div>
         <div class="form-group">
-            <button class="btn btn-default" type="submit">
+            <input class="btn btn-danger" name="password" placeholder="Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class ="btn btn-danger" name ="confirm" placeholder="Confirmation" type="password"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
                 Register
             </button>
